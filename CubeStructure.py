293171ef5af472224e4e8b40195852cd604b56a7
Vincenzo Heska <vincenzo.heska@gmail.com>
2019-02-21 11:23:11 -0500

    Added files from local storage

---
+++ CubeStructure.py
@@ -0,0 +1,154 @@
+import random
+
+class Cube:
+    def __init__(self, clist):
+        assert type(clist) == type([])
+        assert len(clist) == 24
+        self.state = clist
+    def __eq__(self, other):
+        return self.state == other.state
+    def copy(self):
+        return Cube(self.state.copy())
+    def R(self):
+        temp = self.state[1]
+        self.state[1] = self.state[7]
+        self.state[7] = self.state[17]
+        self.state[17] = self.state[21]
+        self.state[21] = temp
+        temp = self.state[3]
+        self.state[3] = self.state[13]
+        self.state[13] = self.state[19]
+        self.state[19] = self.state[23]
+        self.state[23] = temp
+        temp = self.state[8]
+        self.state[8] = self.state[14]
+        self.state[14] = self.state[15]
+        self.state[15] = self.state[9]
+        self.state[9] = temp
+
+    def Ri(self):
+        temp = self.state[21]
+        self.state[21] = self.state[17]
+        self.state[17] = self.state[7]
+        self.state[7] = self.state[1]
+        self.state[1] = temp
+        temp = self.state[23]
+        self.state[23] = self.state[19]
+        self.state[19] = self.state[13]
+        self.state[13] = self.state[3]
+        self.state[3] = temp
+        temp = self.state[9]
+        self.state[9] = self.state[15]
+        self.state[15] = self.state[14]
+        self.state[14] = self.state[8]
+        self.state[8] = temp
+        
+    def F(self):
+        temp = self.state[2]
+        self.state[2] = self.state[11]
+        self.state[11] = self.state[17]
+        self.state[17] = self.state[8]
+        self.state[8] = temp
+        temp = self.state[3]
+        self.state[3] = self.state[5]
+        self.state[5] = self.state[16]
+        self.state[16] = self.state[14]
+        self.state[14] = temp
+        temp = self.state[6]
+        self.state[6] = self.state[12]
+        self.state[12] = self.state[13]
+        self.state[13] = self.state[7]
+        self.state[7] = temp
+
+    def Fi(self):
+        temp = self.state[8]
+        self.state[8] = self.state[17]
+        self.state[17] = self.state[11]
+        self.state[11] = self.state[2]
+        self.state[2] = temp
+        temp = self.state[14]
+        self.state[14] = self.state[16]
+        self.state[16] = self.state[5]
+        self.state[5] = self.state[3]
+        self.state[3] = temp
+        temp = self.state[7]
+        self.state[7] = self.state[13]
+        self.state[13] = self.state[12]
+        self.state[12] = self.state[6]
+        self.state[6] = temp
+        
+    def U(self):
+        temp = self.state[0]
+        self.state[0] = self.state[2]
+        self.state[2] = self.state[3]
+        self.state[3] = self.state[1]
+        self.state[1] = temp
+        temp = self.state[4]
+        self.state[4] = self.state[6]
+        self.state[6] = self.state[8]
+        self.state[8] = self.state[23]
+        self.state[23] = temp
+        temp = self.state[5]
+        self.state[5] = self.state[7]
+        self.state[7] = self.state[9]
+        self.state[9] = self.state[22]
+        self.state[22] = temp
+
+    def Ui(self):
+        temp = self.state[1]
+        self.state[1] = self.state[3]
+        self.state[3] = self.state[2]
+        self.state[2] = self.state[0]
+        self.state[0] = temp
+        temp = self.state[23]
+        self.state[23] = self.state[8]
+        self.state[8] = self.state[6]
+        self.state[6] = self.state[4]
+        self.state[4] = temp
+        temp = self.state[22]
+        self.state[22] = self.state[9]
+        self.state[9] = self.state[7]
+        self.state[7] = self.state[5]
+        self.state[5] = temp
+    def solved(self):
+        return self.state[0] == self.state[1] == self.state[2] == self.state[3]\
+        and self.state[4] == self.state[5] == self.state[10] == self.state[11]\
+        and self.state[6] == self.state[7] == self.state[12] == self.state[13]
+
+movelist = [Cube.R, Cube.Ri, Cube.F, Cube.Fi, Cube.U, Cube.Ui]
+moveliststr = ["R", "Ri", "F", "Fi", "U", "Ui"]
+
+#def brutesolve(cube):
+#    PQ = [[cube, []]]
+#    visited = [cube]
+#    while True:
+#        curPair = PQ.pop(0)
+#        curCube = curPair[0]
+#        curSeq = curPair[1]
+#        for i in range(6):
+#            newSeq = curSeq.copy()
+#            newSeq.append(moveliststr[i])
+#            newCube = curCube.copy()
+#            movelist[i](newCube)
+#            if newCube.solved():
+#                return newSeq
+#            if newCube in visited:
+#                pass
+#            else:
+#                 PQ.append([newCube.copy(), newSeq])
+                
+def randomcube(n):
+    SolvedCube = Cube([0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 1, 1, 2, 2, 3, 3, 4, 4, 4, 4,
+                   5, 5, 5, 5])
+    A = SolvedCube.copy()
+    scramble = []
+    for i in range(n):     
+        r = random.randrange(6)
+        scramble.append(moveliststr[r])
+        movelist[r](A)
+    return (scramble, A)
+
+def solvedcube():
+    SolvedCube = Cube([0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 1, 1, 2, 2, 3, 3, 4, 4, 4, 4,
+                   5, 5, 5, 5])
+    return SolvedCube
---
+++ Solver.py
@@ -0,0 +1,99 @@
+import CubeStructure as cs
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from sklearn import metrics
+from tensorflow.python.data import Dataset
+tf.logging.set_verbosity(tf.logging.ERROR)
+def get_features(cube):
+    d = {}
+    for i in range(24):
+        d[str(i)] = [cube.state[i]]
+    processed_features = pd.DataFrame(data = d)
+    return processed_features
+
+def make_targets():
+    output_targets = pd.DataFrame()
+    output_targets['movesaway'] = 14
+
+def construct_feature_columns(input_features):
+  """Construct the TensorFlow Feature Columns.
+
+  Args:
+    input_features: The names of the numerical input features to use.
+  Returns:
+    A set of feature columns
+  """
+
+
+  
+  return set([tf.feature_column.indicator_column(
+      tf.feature_column.categorical_column_with_identity(
+          key = my_feature,
+          num_buckets = 6))
+              for my_feature in input_features])
+    
+
+def my_input_fn(features):
+    features = {key:np.array(value) for key,value in dict(features).items()}
+    
+    ds = Dataset.from_tensor_slices(features)
+    ds = ds.batch(1)
+
+    features = ds.make_one_shot_iterator().get_next()
+ 
+    return features
+
+def train_nn_regression_model(
+    hidden_units,
+    training_example):
+    pass
+
+def get_cube_score(cube, regressor):
+    features = get_features(cube)
+    predict_input_fn = lambda: my_input_fn(features)
+    predictions = regressor.predict(predict_input_fn)
+    predictions = np.array([item['predictions'][0] for item in predictions])
+    return predictions[0]
+
+def make_dnn_regressor():
+    features = get_features(cs.solvedcube())
+    my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+
+    dnn_regressor = tf.estimator.DNNRegressor(
+        hidden_units = [144, 24],
+        feature_columns = construct_feature_columns(features),
+        optimizer = my_optimizer)
+    return dnn_regressor
+
+def weighted_bfs(cube, regressor):
+    PQ = [[get_cube_score(cube, regressor), cube, []]]
+    visited = [cube]
+    while True:
+        curPair = PQ.pop(0)
+        print(curPair[2], curPair[0])
+        curCube = curPair[1]
+        curSeq = curPair[2]
+        for i in range(6):
+            newSeq = curSeq.copy()
+            newSeq.append(cs.moveliststr[i])
+            newCube = curCube.copy()
+            cs.movelist[i](newCube)
+            if newCube.solved():
+                print(newSeq)
+                return newSeq
+            if newCube in visited:
+                pass
+            else:
+                PQ.append([get_cube_score(cube, regressor), newCube.copy(), newSeq])
+                PQ.sort()
+                visited.append(newCube.copy())
+        if curPair[0] < 1:
+            regressor.
+
+dnn_regressor = make_dnn_regressor()
+
+(scramble, random_cube) = cs.randomcube()
+
+seq = weighted_bfs(random_cube, dnn_regressor)
